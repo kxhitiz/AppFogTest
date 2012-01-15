1484a21c3e1daaa99d72f41b9e53364edd6b4cd2
kxhitiz <kxhitiz@kxhitiz-Studio-1435.(none)>
2012-01-15 21:40:18 +0545

    initialized

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppFogTest::Application.config.secret_token = 'd39d0b8d4c3e9f67ff377987a7da531aaae19b5ad83e8ddbd661b2957770e5ba851cc9879c3fb2513752d2638b4ffd744d32c7b5bf36fbe1ccd829225441eef8'
